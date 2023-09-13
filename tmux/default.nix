4f376de2ac2f24d0fc97f3c689681852cbef62e8
Tobias Langendorf <junglerobba@jngl.one>
2023-09-13 18:40:06 +0200

    tmux: use alt+hjkl for pane navigation

--- tmux/default.nix
+++ tmux/default.nix
@@ -7,9 +7,6 @@
     keyMode = "vi";
     mouse = true;
     extraConfig = ''
-      set -ga terminal-overrides ",screen-256color*:Tc"
-      set-option -g default-terminal "screen-256color"
-
       set -g status-style 'bg=default fg=default'
       set -g status-left-length 25
       set -g status-left '#{=20:session_name} / '
@@ -19,6 +16,11 @@
 
       bind-key -r C-f display-popup -E "tms"
       bind-key -r f display-popup -E "tms switch"
+
+      bind -n M-h select-pane -L
+      bind -n M-j select-pane -D
+      bind -n M-k select-pane -U
+      bind -n M-l select-pane -R
     '';
   };
 }
