3c6cdf0050d57128b6239f1a89ab7e3bebfe8e75
Tobias Langendorf <junglerobba@jngl.one>
2024-01-10 22:03:08 +0100

    tmux: "better" bindings

--- tmux/default.nix
+++ tmux/default.nix
@@ -6,6 +6,7 @@
     escapeTime = 0;
     keyMode = "vi";
     mouse = true;
+    prefix = "C-Space";
     extraConfig = ''
       set -ga terminal-overrides ",screen-256color*:Tc"
       set-option -g default-terminal "screen-256color"
@@ -17,14 +18,22 @@
       set -g status-right-length 100
       set -g status-right "#{?window_bigger,[#{window_offset_x}#,#{window_offset_y}] ,} #{=21:pane_title} %a %F %T"
 
-      bind-key -r C-f display-popup -E "tms"
-      bind-key -r f display-popup -E "tms switch"
-      bind-key -r Q run-shell "tms kill"
+      bind -r C-f display-popup -E "tms"
+      bind -r f display-popup -E "tms switch"
+      bind -r Q run-shell "tms kill"
+
+      bind -r v split-window -h
+      bind -r s split-window -v
 
       bind -n M-h select-pane -L
       bind -n M-j select-pane -D
       bind -n M-k select-pane -U
       bind -n M-l select-pane -R
+
+      bind -r C-h resize-pane -L
+      bind -r C-j resize-pane -D
+      bind -r C-k resize-pane -U
+      bind -r C-l resize-pane -R
     '';
   };
 }
