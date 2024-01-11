a8cd8f00ea3952cefc51fa1d5540faad60050dd4
Tobias Langendorf <junglerobba@jngl.one>
2024-01-11 21:31:08 +0100

    tmux: preserve dir when splitting panes / terminal config

--- tmux/default.nix
+++ tmux/default.nix
@@ -2,15 +2,16 @@
   programs.tmux = {
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
+    terminal = "screen-256color";
     baseIndex = 1;
     escapeTime = 0;
     keyMode = "vi";
     mouse = true;
     prefix = "C-Space";
-    extraConfig = ''
-      set -ga terminal-overrides ",screen-256color*:Tc"
-      set-option -g default-terminal "screen-256color"
 
+    plugins = with pkgs.tmuxPlugins; [ yank ];
+
+    extraConfig = ''
       set -g status-style 'bg=default fg=default'
       set -g status-left-length 25
       set -g status-left '#{=20:session_name} / '
@@ -22,8 +23,8 @@
       bind -r f display-popup -E "tms switch"
       bind -r Q run-shell "tms kill"
 
-      bind -r v split-window -h
-      bind -r s split-window -v
+      bind -r v split-window -h -c "#{pane_current_path}"
+      bind -r s split-window -v -c "#{pane_current_path}"
 
       bind -n M-h select-pane -L
       bind -n M-j select-pane -D
@@ -34,6 +35,9 @@
       bind -r C-j resize-pane -D
       bind -r C-k resize-pane -U
       bind -r C-l resize-pane -R
+
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi C-v send-keys -X rectangle-toggle
     '';
   };
 }
