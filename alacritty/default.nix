3a369e03057ad3b795091eba8af92500fa726d2f
Tobias Langendorf <junglerobba@jngl.one>
2024-09-20 19:20:14 +0200

    alacritty: adjust mac config

--- alacritty/default.nix
+++ alacritty/default.nix
@@ -1,10 +1,15 @@
-{ lib, desktop, ... }:
+{
+  lib,
+  desktop,
+  isMac,
+  ...
+}:
 lib.mkIf (desktop != "gnome") {
   programs.alacritty = {
     enable = true;
     settings = {
       window = {
-        decorations = "None";
+        decorations = lib.mkIf (!isMac) "None";
         padding = {
           x = 5;
           y = 5;
@@ -12,7 +17,7 @@ lib.mkIf (desktop != "gnome") {
       };
       font = {
         normal = {
-          family = "jetbrains mono";
+          family = if isMac then "monaco" else "jetbrains mono";
         };
         size = 10.6;
         offset.y = 3;
