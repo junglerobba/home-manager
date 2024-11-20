fc27703e3134f4db1d417e18eceae7d43dc59ed2
Tobias Langendorf <junglerobba@jngl.one>
2024-11-21 00:11:41 +0100

    fish: add jj integration

--- fish/default.nix
+++ fish/default.nix
@@ -71,6 +71,56 @@
             export DOCKER_BUILDKIT=0
           ''
         ];
+
+      # from https://gist.github.com/hroi/d0dc0e95221af858ee129fd66251897e
+      fish_jj_prompt = ''
+        # Is jj installed?
+        if not command -sq jj
+            return 1
+        end
+
+        # Are we in a jj repo?
+        if not jj root --quiet &>/dev/null
+            return 1
+        end
+
+        # Generate prompt
+        jj log --ignore-working-copy --no-graph --color always -r @ -T '
+            surround(
+                " (",
+                ")",
+                separate(
+                    " ",
+                    bookmarks.join(", "),
+                    coalesce(
+                        surround(
+                            "\"",
+                            "\"",
+                            if(
+                                description.first_line().substr(0, 24).starts_with(description.first_line()),
+                                description.first_line().substr(0, 24),
+                                description.first_line().substr(0, 23) ++ "…"
+                            )
+                        ),
+                        "(no description set)"
+                    ),
+                    change_id.shortest(),
+                    commit_id.shortest(),
+                    if(conflict, "(conflict)"),
+                    if(empty, "(empty)"),
+                    if(divergent, "(divergent)"),
+                    if(hidden, "(hidden)"),
+                )
+            )
+        '
+      '';
+
+      fish_vcs_prompt = ''
+        fish_jj_prompt $argv
+        or fish_git_prompt $argv
+        or fish_hg_prompt $argv
+        or fish_fossil_prompt $argv
+      '';
     };
 
     shellInit = ''
