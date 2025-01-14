fe26401009d5f927e7100b8d994d285faa61b8a5
Tobias Langendorf <junglerobba@jngl.one>
2025-01-14 11:32:57 +0100

    remove colima

--- colima/default.nix
+++
@@ -1,2 +0,0 @@
-{ lib, isMac, ... }:
-lib.mkIf isMac { home.file.".colima/_templates/default.yaml".source = ./template.yml; }
--- home.nix
+++ home.nix
@@ -64,7 +64,6 @@ in
 
   imports = [
     ./alacritty
-    ./colima
     ./fish
     ./git
     ./gnome
