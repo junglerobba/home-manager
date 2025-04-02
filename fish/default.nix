747de3900303414d025d51ad0a28630135004a1e
j <junglerobba@jngl.one>
2025-04-03 00:03:12 +0200

    fish: show closest "branch" in jj prompt

--- fish/default.nix
+++ fish/default.nix
@@ -72,47 +72,38 @@
           ''
         ];
 
-      # from https://gist.github.com/hroi/d0dc0e95221af858ee129fd66251897e
+      # adapted from https://gist.github.com/hroi/d0dc0e95221af858ee129fd66251897e
       fish_jj_prompt = ''
-        # Is jj installed?
-        if not command -sq jj
-            return 1
-        end
+        # Defined via `source`
+        function fish_jj_prompt
+            # Is jj installed?
+            if not command -sq jj
+                return 1
+            end
 
-        # Are we in a jj repo?
-        if not jj root --quiet &>/dev/null
-            return 1
-        end
+            # Are we in a jj repo?
+            if not jj root --quiet &>/dev/null
+                return 1
+            end
 
-        # Generate prompt
-        jj log --ignore-working-copy --no-graph --color always -r @ -T '
-            surround(
-                " (",
-                ")",
+            # Generate prompt
+            set -l branch (jj log --ignore-working-copy --no-graph --color always -r 'closest_bookmark(@)' -T 'bookmarks.join(", ")')
+            set -l info
+            if test -n $branch
+                set -a info $branch
+            end
+            set -a info (jj log --ignore-working-copy --no-graph --color always -r @ -T '
                 separate(
                     " ",
-                    bookmarks.join(", "),
-                    coalesce(
-                        surround(
-                            "\"",
-                            "\"",
-                            if(
-                                description.first_line().substr(0, 24).starts_with(description.first_line()),
-                                description.first_line().substr(0, 24),
-                                description.first_line().substr(0, 23) ++ "…"
-                            )
-                        ),
-                        "(no description set)"
-                    ),
                     change_id.shortest(),
-                    commit_id.shortest(),
                     if(conflict, "(conflict)"),
                     if(empty, "(empty)"),
                     if(divergent, "(divergent)"),
                     if(hidden, "(hidden)"),
                 )
-            )
-        '
+            ')
+            echo " ($(string join ' ' $info))"
+        end
       '';
 
       fish_vcs_prompt = ''
