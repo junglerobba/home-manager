52da10cc46af2dc9afd650f4f30652958a86868d
j <junglerobba@jngl.one>
2025-04-08 23:27:09 +0200

    jj: add config

--- jujutsu/default.nix
+++ jujutsu/default.nix
@@ -1,6 +1,31 @@
 { ... }:
+let
+  aliases = {
+    closest_bookmark = rev: "closest_bookmark(${rev})";
+    tip = rev: "tip(${rev})";
+  };
+in
 {
   programs.jujutsu = {
     enable = true;
+    settings = {
+      ui = {
+        default-command = "log";
+      };
+      revset-aliases = with aliases; {
+        "${closest_bookmark "to"}" = "heads(::to & bookmarks())";
+        "${tip "branch"}" = "heads(::branch & ~empty() & ~description(exact:\"\"))";
+      };
+      aliases = with aliases; {
+        tug = [
+          "bookmark"
+          "move"
+          "--from"
+          (closest_bookmark "@-")
+          "--to"
+          (tip "@")
+        ];
+      };
+    };
   };
 }
