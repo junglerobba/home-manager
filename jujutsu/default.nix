354d61802f1dc058e0354e232c2ba35d52ed8c54
j <junglerobba@jngl.one>
2025-05-10 11:15:54 +0200

    jj: fix config file location
    home-manager cannot detect the version of the flake package as >= 0.29.0,
    so it places the config file in the old location. Since it simply turns
    whatever is inside `settings` into toml anyway, this file might as well
    be created normally.

--- jujutsu/default.nix
+++ jujutsu/default.nix
@@ -1,44 +1,61 @@
-{ ... }:
+{ pkgs, ... }:
 let
   aliases = {
     closest_bookmark = rev: "closest_bookmark(${rev})";
     tip = rev: "tip(${rev})";
+    gerrit_change_id = change_id: "gerrit_change_id(${change_id})";
   };
 in
 {
+  xdg.configFile."jj/config.toml".source = (pkgs.formats.toml { }).generate "jj" {
+    ui = {
+      default-command = "log";
+    };
+    git = {
+      write-change-id-header = true;
+    };
+    revset-aliases = with aliases; {
+      "${closest_bookmark "to"}" = "heads(::to & bookmarks())";
+      "${tip "branch"}" = "heads(::branch & ~empty() & ~description(exact:\"\"))";
+    };
+    aliases = with aliases; {
+      tug = [
+        "bookmark"
+        "move"
+        "--from"
+        (closest_bookmark "@-")
+        "--to"
+        (tip "@")
+      ];
+    };
+    template-aliases = with aliases; {
+      "${gerrit_change_id "change_id"}" = ''
+        "Change-Id: I6a6a6964" ++ change_id.normal_hex()
+      '';
+    };
+    templates = {
+      draft_commit_description = ''
+        concat(
+          coalesce(description, "\n"),
+          surround(
+            "\nJJ: This commit contains the following changes:\n", "",
+            indent("JJ:     ", diff.stat(72)),
+          ),
+          "\nJJ: ignore-rest\n",
+          diff.git(),
+        )
+      '';
+      commit_trailers = ''
+        if(
+          config("gerrit.enabled").as_boolean(),
+          gerrit_change_id(change_id)
+        )
+      '';
+    };
+    # only exists to override
+    gerrit.enabled = false;
+  };
   programs.jujutsu = {
     enable = true;
-    settings = {
-      ui = {
-        default-command = "log";
-      };
-      revset-aliases = with aliases; {
-        "${closest_bookmark "to"}" = "heads(::to & bookmarks())";
-        "${tip "branch"}" = "heads(::branch & ~empty() & ~description(exact:\"\"))";
-      };
-      aliases = with aliases; {
-        tug = [
-          "bookmark"
-          "move"
-          "--from"
-          (closest_bookmark "@-")
-          "--to"
-          (tip "@")
-        ];
-      };
-      templates = {
-        draft_commit_description = ''
-          concat(
-            coalesce(description, "\n"),
-            surround(
-              "\nJJ: This commit contains the following changes:\n", "",
-              indent("JJ:     ", diff.stat(72)),
-            ),
-            "\nJJ: ignore-rest\n",
-            diff.git(),
-          )
-        '';
-      };
-    };
   };
 }
