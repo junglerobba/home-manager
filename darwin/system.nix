2ef18487751524786f1d74b200c392eec0a9ec10
junglerobba <junglerobba@jngl.one>
2025-07-12 19:47:54 +0200

    darwin: add defaults config

---
+++ darwin/system.nix
@@ -0,0 +1,52 @@
+{
+  ...
+}:
+{
+  system = {
+
+    defaults = {
+      controlcenter = {
+        Bluetooth = true;
+        NowPlaying = true;
+        Sound = true;
+      };
+      dock = {
+        autohide = true;
+        # Whether to automatically rearrange spaces based on most recent use
+        mru-spaces = false;
+        show-recents = false;
+        persistent-others = [
+          "/Applications/Nix Apps"
+        ];
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        ShowPathbar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      menuExtraClock = {
+        Show24Hour = true;
+        ShowDate = 1; # 0 = When space allows 1 = Always 2 = Never
+        ShowSeconds = true;
+      };
+      WindowManager = {
+        EnableStandardClickToShowDesktop = false;
+      };
+      NSGlobalDomain = {
+        AppleInterfaceStyleSwitchesAutomatically = true;
+      };
+      ActivityMonitor = {
+        ShowCategory = 101; # All Processes, Hierarchally
+      };
+      screencapture.target = "clipboard";
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+  };
+}
