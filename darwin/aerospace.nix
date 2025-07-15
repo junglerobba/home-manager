60972cd6b27e016c0b191b63862ce3adeecdbc25
junglerobba <junglerobba@jngl.one>
2025-07-15 09:36:16 +0200

    aerospace: use cmd for more bindings
    Moving stuff to workspaces shouldn't be needed that often, so it's not
    a problem to have these on alt instead of cmd

--- darwin/aerospace.nix
+++ darwin/aerospace.nix
@@ -15,16 +15,16 @@
         cmd-shift-k = "move up";
         cmd-shift-l = "move right";
 
-        alt-1 = "workspace 1";
-        alt-2 = "workspace 2";
-        alt-3 = "workspace 3";
-        alt-4 = "workspace 4";
-        alt-5 = "workspace 5";
-        alt-6 = "workspace 6";
-        alt-7 = "workspace 7";
-        alt-8 = "workspace 8";
-        alt-9 = "workspace 9";
-        alt-0 = "workspace 10";
+        cmd-1 = "workspace 1";
+        cmd-2 = "workspace 2";
+        cmd-3 = "workspace 3";
+        cmd-4 = "workspace 4";
+        cmd-5 = "workspace 5";
+        cmd-6 = "workspace 6";
+        cmd-7 = "workspace 7";
+        cmd-8 = "workspace 8";
+        cmd-9 = "workspace 9";
+        cmd-0 = "workspace 10";
         alt-shift-1 = "move-node-to-workspace 1";
         alt-shift-2 = "move-node-to-workspace 2";
         alt-shift-3 = "move-node-to-workspace 3";
@@ -36,11 +36,11 @@
         alt-shift-9 = "move-node-to-workspace 9";
         alt-shift-0 = "move-node-to-workspace 10";
 
-        alt-period = "focus-monitor next";
-        alt-comma = "focus-monitor prev";
+        cmd-period = "focus-monitor next";
+        cmd-comma = "focus-monitor prev";
 
-        alt-shift-period = "move-workspace-to-monitor next";
-        alt-shift-comma = "move-workspace-to-monitor prev";
+        cmd-shift-period = "move-workspace-to-monitor next";
+        cmd-shift-comma = "move-workspace-to-monitor prev";
 
         alt-w = "layout h_tiles h_accordion";
       };
