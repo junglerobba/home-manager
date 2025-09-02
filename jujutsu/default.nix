3bb19eff327223bc32c588ec86a4ac1956e4b404
junglerobba <junglerobba@jngl.one>
2025-09-02 11:55:12 +0200

    jj: automatically set feature branch name for jira ticket ids

--- jujutsu/default.nix
+++ jujutsu/default.nix
@@ -8,6 +8,7 @@ let
     closest_bookmark = rev: "closest_bookmark(${rev})";
     tip = rev: "tip(${rev})";
     gerrit_change_id = change_id: "gerrit_change_id(${change_id})";
+    jira_ticket_id = description: "jira_ticket_id(${description})";
   };
 in
 {
@@ -74,6 +75,9 @@ in
         "${gerrit_change_id "change_id"}" = ''
           "Change-Id: I6a6a6964" ++ change_id.normal_hex()
         '';
+        "${jira_ticket_id "description"}" = ''
+          description.match(regex:"^\\b[A-Z][A-Z0-9_]+-[1-9][0-9]*")
+        '';
       };
       templates = {
         draft_commit_description = ''
@@ -85,6 +89,19 @@ in
             ),
             "\nJJ: ignore-rest\n",
             diff.git(),
+          )
+        '';
+        git_push_bookmark = ''
+          if(
+            jira_ticket_id(description).len() > 0,
+            concat(
+              "feature/",
+              jira_ticket_id(description)
+            ),
+            concat(
+              "push-",
+              change_id.short()
+            )
           )
         '';
         commit_trailers = ''
