92d31bf65d386be020fbfcad87fc480421be960e
junglerobba <junglerobba@jngl.one>
2025-10-02 20:00:56 +0200

    jj: update to v0.34.0

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     helix.url = "github:helix-editor/helix";
-    jj.url = "github:jj-vcs/jj?ref=v0.33.0";
+    jj.url = "github:jj-vcs/jj?ref=v0.34.0";
   };
 
   outputs =
