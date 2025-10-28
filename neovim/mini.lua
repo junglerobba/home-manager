b422d1d0996d8a8126e63919a2414739c53733b6
junglerobba <junglerobba@jngl.one>
2025-10-28 18:23:05 +0100

    nvim/mini: add modified files picker

--- neovim/mini.lua
+++ neovim/mini.lua
@@ -1,63 +1,64 @@
-require('mini.ai').setup()         -- a/i textobjects
-require('mini.align').setup()      -- aligning
-require('mini.bracketed').setup()  -- unimpaired bindings with TS
-require('mini.comment').setup()    -- TS-wise comments
-require('mini.jump').setup()       -- fFtT work past a line
-require('mini.pairs').setup()      -- pair brackets
-require('mini.surround').setup({   -- surround
-  custom_surroundings = {
-    ['l'] = { output = { left = '[', right = ']()'}}
-  }
+require('mini.ai').setup()        -- a/i textobjects
+require('mini.align').setup()     -- aligning
+require('mini.bracketed').setup() -- unimpaired bindings with TS
+require('mini.comment').setup()   -- TS-wise comments
+require('mini.jump').setup()      -- fFtT work past a line
+require('mini.pairs').setup()     -- pair brackets
+require('mini.surround').setup({  -- surround
+    custom_surroundings = {
+        ['l'] = { output = { left = '[', right = ']()' } }
+    }
 })
 local miniclue = require('mini.clue')
-miniclue.setup({                   -- cute prompts about bindings
-  triggers = {
-    { mode = 'n', keys = '<Leader>' },
-    { mode = 'x', keys = '<Leader>' },
-    { mode = 'n', keys = '<space>' },
-    { mode = 'x', keys = '<space>' },
+miniclue.setup({ -- cute prompts about bindings
+    triggers = {
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+        { mode = 'n', keys = '<space>' },
+        { mode = 'x', keys = '<space>' },
 
-    -- Built-in completion
-    { mode = 'i', keys = '<C-x>' },
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
 
-    -- `g` key
-    { mode = 'n', keys = 'g' },
-    { mode = 'x', keys = 'g' },
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
 
-    -- Marks
-    { mode = 'n', keys = "'" },
-    { mode = 'n', keys = '`' },
-    { mode = 'x', keys = "'" },
-    { mode = 'x', keys = '`' },
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
 
-    -- Registers
-    { mode = 'n', keys = '"' },
-    { mode = 'x', keys = '"' },
-    { mode = 'i', keys = '<C-r>' },
-    { mode = 'c', keys = '<C-r>' },
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
 
-    -- Window commands
-    { mode = 'n', keys = '<C-w>' },
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
 
-    -- `z` key
-    { mode = 'n', keys = 'z' },
-    { mode = 'x', keys = 'z' },
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
 
-    -- Bracketed
-    { mode = 'n', keys = '[' },
-    { mode = 'n', keys = ']' },
-  },
-  clues = {
-    miniclue.gen_clues.builtin_completion(),
-    miniclue.gen_clues.g(),
-    miniclue.gen_clues.marks(),
-    miniclue.gen_clues.registers(),
-    miniclue.gen_clues.windows(),
-    miniclue.gen_clues.z(),
-  },
+        -- Bracketed
+        { mode = 'n', keys = '[' },
+        { mode = 'n', keys = ']' },
+    },
+    clues = {
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+    },
 })
 
 local picker = require('mini.pick')
+local extras = require('mini.extra')
 picker.setup({
     options = {
         content_from_bottom = true,
@@ -106,8 +107,15 @@ picker.setup({
         },
     },
 })
+extras.setup()
 
 vim.keymap.set('n', '<leader>f', picker.builtin.files)
 vim.keymap.set('n', '<leader>b', picker.builtin.buffers)
 vim.keymap.set('n', '<leader>/', picker.builtin.grep_live)
+vim.keymap.set('n', '<leader>g', function()
+    extras.pickers.git_files({
+        scope = "modified",
+    })
+end
+)
 vim.keymap.set('n', "<leader>'", picker.builtin.resume)
