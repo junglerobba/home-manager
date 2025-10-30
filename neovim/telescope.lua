4729283d62a93ffdf43b364d4432578fda1a3848
junglerobba <junglerobba@jngl.one>
2025-10-30 13:17:06 +0100

    nvim: remove unused telescope config

--- neovim/telescope.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<space>/', "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap = true, silent = true, desc = "Live Grep"})
-vim.keymap.set('n', '<space>f', ":lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap = true, silent = true, desc = "Live Grep"})
-vim.keymap.set('n', '<space>b', ":lua require('telescope.builtin').buffers()<cr>", {noremap = true, silent = true, desc = "Buffers"})
