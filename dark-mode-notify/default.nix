16d3b0f99bd9cd376cb49a574237de2315705ffe
junglerobba <junglerobba@jngl.one>
2025-12-03 18:02:02 +0100

    dark-mode-notify: fix standalone home-manager version
    mkIf doesn't generate the correct attrset in this case, and
    substituteAll was removed

--- dark-mode-notify/default.nix
+++ dark-mode-notify/default.nix
@@ -8,8 +8,7 @@
 }:
 let
   script = pkgs.alacritty-theme-toggle;
-  plist = pkgs.substituteAll {
-    src = ./ke.bou.dark-mode-notify.plist;
+  plist = pkgs.replaceVars ./ke.bou.dark-mode-notify.plist {
     bin = lib.getExe pkgs.dark-mode-notify;
     arguments = lib.concatStringsSep "\n" (
       builtins.map (arg: "<string>${arg}</string>") [
@@ -19,29 +18,31 @@ let
   };
   path = "Library/LaunchAgents/ke.bou.dark-mode-notify.plist";
 in
-# for standalone home-manager usage on mac without nix-darwin
-lib.mkIf (isMac && !darwin) {
-  home.file.${path}.source = plist;
-  home.activation.dark-mode-notify = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    run launchctl unload ~/${path}
-    run launchctl load -w ~/${path}
-  '';
-}
-// lib.mkIf isLinux {
-  systemd.user.services.dark-mode-notify = {
-    Unit = {
-      Description = "Monitor dark mode setting";
-      After = [ "graphical-session.target" ];
+lib.mkMerge [
+  # for standalone home-manager usage on mac without nix-darwin
+  (lib.mkIf (isMac && !darwin) {
+    home.file.${path}.source = plist;
+    home.activation.dark-mode-notify = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run /bin/launchctl unload ~/${path} || true
+      run /bin/launchctl load -w ~/${path}
+    '';
+  })
+  (lib.mkIf isLinux {
+    systemd.user.services.dark-mode-notify = {
+      Unit = {
+        Description = "Monitor dark mode setting";
+        After = [ "graphical-session.target" ];
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
+      Service = {
+        ExecStart = ''
+          ${lib.getExe pkgs.dbus-settings-portal-monitor} \
+            --key org.freedesktop.appearance \
+            --setting color-scheme \
+            --env DARKMODE \
+            --cmd ${script}
+        '';
+      };
     };
-    Install.WantedBy = [ "graphical-session.target" ];
-    Service = {
-      ExecStart = ''
-        ${lib.getExe pkgs.dbus-settings-portal-monitor} \
-          --key org.freedesktop.appearance \
-          --setting color-scheme \
-          --env DARKMODE \
-          --cmd ${script}
-      '';
-    };
-  };
-}
+  })
+]
