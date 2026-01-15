d77a645125ba31c537c66b81cd955697cbdcb3c6
junglerobba <junglerobba@jngl.one>
2026-01-15 08:47:49 +0100

    nvim/oil: setup columns and view options

--- neovim/oil.lua
+++ neovim/oil.lua
@@ -1,4 +1,14 @@
 local oil = require('oil')
-oil.setup()
+oil.setup({
+    columns = {
+        "icon",
+        "permissions",
+    },
+    skip_confirm_for_simple_edits = true,
+    watch_for_changes = true,
+    view_options = {
+        show_hidden = true,
+    },
+})
 
 vim.keymap.set('n', '<leader>e', oil.open)
