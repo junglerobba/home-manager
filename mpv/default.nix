02968fa6a696633709ef0ac5e9f7a879473e375e
junglerobba <junglerobba@jngl.one>
2026-07-19 11:29:32 +0200

    mpv: install on all systems

--- mpv/default.nix
+++ mpv/default.nix
@@ -1,30 +1,28 @@
 {
   pkgs,
   isLinux,
-  isNixOs,
   ...
 }:
 {
   programs.mpv = {
-    enable = isNixOs;
-    config =
-      {
-        keep-open = true;
-        audio-display = false;
-        osd-font = "roboto";
-        osd-font-size = 15;
-        sub-font = "roboto";
-        sub-font-size = 36;
-      }
-      // (
-        if isLinux then
-          {
-            vo = "dmabuf-wayland";
-            gpu-context = "wayland";
-            hwdec = "vaapi";
-          }
-        else
-          { }
-      );
+    enable = true;
+    config = {
+      keep-open = true;
+      audio-display = false;
+      osd-font = "roboto";
+      osd-font-size = 15;
+      sub-font = "roboto";
+      sub-font-size = 36;
+    }
+    // (
+      if isLinux then
+        {
+          vo = "dmabuf-wayland";
+          gpu-context = "wayland";
+          hwdec = "vaapi";
+        }
+      else
+        { }
+    );
   };
 }
