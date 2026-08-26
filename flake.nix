f5fc002ee9a4376e647b07d403bcb1c2762d1fa0
Tobias Langendorf <junglerobba@jngl.one>
2026-08-26 19:25:05 +0200

    add helix cachix instance to flake config
    will not be picked up when importing this flake, or when building via
    home-manager, but I should get in the habit of building this with `nix
    flake check --accept-flake-config` before pushing anyway, and that way
    it can be pulled from cache beforehand, and then home-manager doesn't
    have much left to build afterwards.

--- flake.nix
+++ flake.nix
@@ -114,4 +114,9 @@
         };
       }
     );
+
+  nixConfig = {
+    extra-substituters = [ "https://helix.cachix.org" ];
+    extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
+  };
 }
